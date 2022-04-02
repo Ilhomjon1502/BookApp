a0857176fd4391638e71ca93922124e645a70612
dell <IlhomjonIbragimov1502@gmail.com>
2022-04-02 12:17:38 +0500

    ready1

--- app/src/main/java/uz/ilhomjon/bookapp/view/MainActivity2.kt
+++ app/src/main/java/uz/ilhomjon/bookapp/view/MainActivity2.kt
@@ -23,97 +23,67 @@ class MainActivity2 : AppCompatActivity() {
 
         isCategory = intent.getIntExtra("isCategory", -1)
 
-        if (isCategory == 1){
-            val book = intent.getSerializableExtra("keyBook") as Book
-            binding.apply {
-                Picasso.get().load(book.book_image).into(image2)
-                tvDisplayName1.text = book.title
-                tvDisplayName2.text = book.author
-                tvRating.text = book.rank.toString()
-                tvPrice.text = book.price
-                tvDesc.text = book.description
-
-                imageSave.setOnClickListener {
-                    val saveBook = SaveBook(book.title, book.author, book.rank, book.price, book.description, book.book_image)
-                    if (list.contains(saveBook)){
-                        list.remove(saveBook)
-                        MySharedPreference.obektString = list
-                        Toast.makeText(this@MainActivity2, "Saqlanganlardan o'chirildi", Toast.LENGTH_SHORT).show()
-                    }else{
-                        list.add(saveBook)
-                        MySharedPreference.obektString = list
-                        Toast.makeText(this@MainActivity2, "Saqlanganlarga qo'sjildi", Toast.LENGTH_SHORT).show()
-                    }
-                }
+        when(isCategory){
+            0->{
+                val book = intent.getSerializableExtra("keyBook") as uz.ilhomjon.bookapp.Models.myclass.AllBook.Book
+                val saveBook = SaveBook(
+                    book.title,
+                    book.author,
+                    book.rank,
+                    book.price,
+                    book.description,
+                    book.book_image
+                )
+                saving(saveBook)
             }
-        }else if (isCategory == 0){
-            val book = intent.getSerializableExtra("keyBook") as uz.ilhomjon.bookapp.Models.myclass.AllBook.Book
-            binding.apply {
-                Picasso.get().load(book.book_image).into(image2)
-                tvDisplayName1.text = book.title
-                tvDisplayName2.text = book.author
-                tvRating.text = book.rank.toString()
-                tvPrice.text = book.price
-                tvDesc.text = book.description
-
-                imageSave.setOnClickListener {
-                    val saveBook = SaveBook(book.title, book.author, book.rank, book.price, book.description, book.book_image)
-                    if (list.contains(saveBook)){
-                        list.remove(saveBook)
-                        MySharedPreference.obektString = list
-                        Toast.makeText(this@MainActivity2, "Saqlanganlardan o'chirildi", Toast.LENGTH_SHORT).show()
-                        imageSave.setImageResource(R.drawable.save)
-                    }else{
-                        list.add(saveBook)
-                        MySharedPreference.obektString = list
-                        Toast.makeText(this@MainActivity2, "Saqlanganlarga qo'sjildi", Toast.LENGTH_SHORT).show()
-                        imageSave.setImageResource(R.drawable.ic_savong)
-                    }
-                }
+            1->{
+                val book = intent.getSerializableExtra("keyBook") as Book
+                val saveBook = SaveBook(
+                    book.title,
+                    book.author,
+                    book.rank,
+                    book.price,
+                    book.description,
+                    book.book_image
+                )
+                saving(saveBook)
             }
-        }else if(isCategory == 3) {
-            binding.apply {
-                val book = intent.getSerializableExtra("keyBook") as SaveBook
-                binding.apply {
-                    Picasso.get().load(book.imageLink).into(image2)
-                    tvDisplayName1.text = book.title
-                    tvDisplayName2.text = book.author
-                    tvRating.text = book.rank.toString()
-                    tvPrice.text = book.price
-                    tvDesc.text = book.desc
-                    imageSave.setImageResource(R.drawable.ic_savong)
-
-                    imageSave.setOnClickListener {
-                        val saveBook = SaveBook(
-                            book.title,
-                            book.author,
-                            book.rank,
-                            book.price,
-                            book.desc,
-                            book.imageLink
-                        )
-                        if (list.contains(saveBook)) {
-                            list.remove(saveBook)
-                            MySharedPreference.obektString = list
-                            Toast.makeText(
-                                this@MainActivity2,
-                                "Saqlanganlardan o'chirildi",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        } else {
-                            list.add(saveBook)
-                            MySharedPreference.obektString = list
-                            Toast.makeText(
-                                this@MainActivity2,
-                                "Saqlanganlarga qo'sjildi",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        }
-                    }
-                }
+            3->{
+                saving(intent.getSerializableExtra("keyBook") as SaveBook)
             }
         }
 
         binding.imageBack.setOnClickListener { finish() }
+    }
+
+    fun saving(book:SaveBook){
+        MySharedPreference.init(this)
+        val list = MySharedPreference.obektString
+        binding.apply {
+            if (list.contains(book)){
+                imageSave.setImageResource(R.drawable.ic_savong)
+            }
+            Picasso.get().load(book.imageLink).into(image2)
+            tvDisplayName1.text = book.title
+            tvDisplayName2.text = book.author
+            tvRating.text = book.rank.toString()
+            tvPrice.text = book.price
+            tvDesc.text = book.desc
+
+            imageSave.setOnClickListener {
+                if (list.contains(book)){
+                    list.remove(book)
+                    MySharedPreference.obektString = list
+                    Toast.makeText(this@MainActivity2, "O'chirildi", Toast.LENGTH_SHORT).show()
+                    imageSave.setImageResource(R.drawable.save)
+                }else{
+                    list.add(book)
+                    MySharedPreference.obektString = list
+                    Toast.makeText(this@MainActivity2, "Saqlandi", Toast.LENGTH_SHORT).show()
+                    imageSave.setImageResource(R.drawable.ic_savong)
+                }
+            }
+        }
+
     }
 }
